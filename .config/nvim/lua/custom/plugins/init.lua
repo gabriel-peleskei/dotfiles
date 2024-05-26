aa8dac59ef95ba029d278ee9380d5eee30efd06d
Gabriel Peleskei <gabriel.peleskei@gmail.com>
2024-05-26 18:54:30 +0200

    added some data

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,11 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'tribela/vim-transparent' },
+  { 'gabriel-peleskei/improved-ft', opts = {
+    use_default_mappings = true,
+    ignore_char_case = true,
+  } },
+}
